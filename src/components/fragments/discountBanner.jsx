642c7d16cd8df5a38d18c76687ada98d1d2aa7dc
AldenCoding <alden.f.edria@gmail.com>
2023-11-05 20:57:34 +0700

    add men page

---
+++ src/components/fragments/discountBanner.jsx
@@ -0,0 +1,13 @@
+const DiscountBanner = () => {
+  return (
+    <div className="bg-red-700 w-full">
+      <h3>Lorem ipsum dolor sit amet consectetur.</h3>
+      <span>
+        <a href="">Shop Now</a>
+      </span>
+      <span>Lorem, ipsum dolor sit amet consectetur adipisicing.</span>
+    </div>
+  );
+};
+
+export default DiscountBanner;
